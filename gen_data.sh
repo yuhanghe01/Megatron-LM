641d478fcde79cd014f7ffd5c76d9137026d293e
Yuhang He (Henry) <11781064+yuhanghe01@users.noreply.github.com>
2025-12-29 22:00:18 -0800

    Update output prefix in gen_data.sh

--- gen_data.sh
+++ gen_data.sh
@@ -3,7 +3,7 @@ MERGE_FILE=/mnt/blob-data-sigmasystem/yuhang/Cairn/gpt2_data/merges.txt
 INPUT_FILE=/mnt/blob-data-sigmasystem/yuhang/Cairn/codeparrot_data.json
 python tools/preprocess_data.py \
        --input codeparrot_data.json \
-       --output-prefix /mnt/blob-data-sigmasystem-out/yuhang/Cairn/codeparrot \
+       --output-prefix /mnt/blob-data-sigmasystem-out/yuhang/Cairn/megatron/codeparrot \
        --vocab-file $VOCAB_FILE \
        --tokenizer-type GPT2BPETokenizer \
        --merge-file $MERGE_FILE \
