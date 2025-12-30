8d469656a867f690c1cbacce0bf151a72d982369
Yuhang He (Henry) <11781064+yuhanghe01@users.noreply.github.com>
2025-12-29 21:57:46 -0800

    Add script to preprocess data for GPT-2

---
+++ gen_data.sh
@@ -0,0 +1,12 @@
+VOCAB_FILE=/mnt/blob-data-sigmasystem/yuhang/Cairn/gpt2_data/vocab.json
+MERGE_FILE=/mnt/blob-data-sigmasystem/yuhang/Cairn/gpt2_data/merges.txt
+INPUT_FILE=/mnt/blob-data-sigmasystem/yuhang/Cairn/codeparrot_data.json
+python tools/preprocess_data.py \
+       --input codeparrot_data.json \
+       --output-prefix /mnt/blob-data-sigmasystem-out/yuhang/Cairn/codeparrot \
+       --vocab-file $VOCAB_FILE \
+       --tokenizer-type GPT2BPETokenizer \
+       --merge-file $MERGE_FILE \
+       --json-keys content \
+       --workers 32 \
+       --append-eod
